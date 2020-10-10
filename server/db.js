509374607dd3669ed3430dbada3591eb0564a2fc
ambientstl <41842065+ambientstl@users.noreply.github.com>
2020-10-10 13:30:03 -0500

    test db url

--- server/db.js
+++ server/db.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 
+console.log(process.env.DATABASE_URL)
+
 const connectionString =
   process.env.DATABASE_URL || 'postgres://postgres@localhost:5432';
 const pool = new Pool({ connectionString });
