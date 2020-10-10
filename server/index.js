6778d4bb2579d05993c338362a518c323f922093
ambientstl <41842065+ambientstl@users.noreply.github.com>
2020-10-10 15:19:05 -0500

    add cors header middleware and change blog request and formatting

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const posts = require('./routers/posts.js');
+const corsHeader = require('./corsHeader');
 
 const appPort = process.env.PORT || 8675;
 const app = express();
 
 app
+  .use(corsHeader)
   .use(bodyParser.json())
   .use(morgan('dev'))
   .use('/posts', posts)
